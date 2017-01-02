f9f63c7f3e6afb11cd8ca8a2753c1074249fc1be
satishjasthi <satish27may@gmail.com>
2017-01-02 23:00:59 +0530

    Use only 1% of data to train SVC

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,6 +19,8 @@ from sklearn.metrics import accuracy_score
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100] 
 
 
 
