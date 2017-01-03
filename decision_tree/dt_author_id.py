37d0146646647ec16631e5d499b911d4de9079ac
satishjasthi <satish27may@gmail.com>
2017-01-03 23:10:59 +0530

    Add decision_tree

---
+++ decision_tree/dt_author_id.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+""" 
+    This is the code to accompany the Lesson 3 (decision tree) mini-project.
+
+    Use a Decision Tree to identify emails from the Enron corpus by author:    
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+#number of features currently using
+feature_length = len(features_train[0])
+print feature_length
+ #min_sample_split is a Decision tree parameter
+
+#########################################################
+### your code goes here ###
+clf = DecisionTreeClassifier(min_samples_split = 40)
+clf.fit(features_train,labels_train)
+pred = clf.predict(features_test)
+accuracy = accuracy_score(labels_test,pred)
+
+print accuracy
+
+#########################################################
+
+
